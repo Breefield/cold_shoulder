7aa6d3886aae08c2e202e581ae1da0424edf4ac3
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-13 15:08:01 -0800

    Move locales and add files properly

--- lib/cold_shoulder.rb
+++ lib/cold_shoulder.rb
@@ -7,7 +7,7 @@ module ColdShoulder
   if defined?(Rails)
     class Railtie < Rails::Railtie
       config.before_configuration do
-        I18n.load_path << File.join(File.dirname(__FILE__), 'locales', 'en.yml')
+        I18n.load_path << File.join(File.dirname(__FILE__), '../', 'locales', 'en.yml')
       end
     end
   end
